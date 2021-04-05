6c16913567e95905b294c9aaee075a8ad94fd506
habib <habib.gani@sunovion.com>
2021-04-04 22:24:50 -0500

    Added stockPrice feature with RobinAPI

--- rh.py
+++ rh.py
@@ -5,9 +5,13 @@ import pprint
 
 login = rh.login("roshan.poduval@gmail.com","testpassword579")
 
-# if you search for 'apple' or 'alphabet', this works, but 'google' doesn't
-search = input("What company do you want to know about?")
+# if you search for 'apple' or 'alphabet' works (NOTE: Google name is not in Robinhood. Only Alphabet.)
+search = input("What company do you want to know about? ")
 stock = rh.stocks.find_instrument_data(search)
 if stock[0] is not None:
     stock = stock[0]['symbol']
+    # get stockPrice as a String
+    stockPrice = ' '.join(rh.stocks.get_latest_price(stock))
+    print("Current price of " + stock + ": $" + stockPrice)
+    print("General information about the " + stock + " stock:")
     pprint.pprint(rh.stocks.get_fundamentals(stock))
