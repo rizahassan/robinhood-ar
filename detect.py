198825a33b5cb094dfcd80b1a2132a76c60740d7
roshanpoduval <roshan.poduval@gmail.com>
2021-04-07 14:55:08 -0500

    Added edge detection
    The stock code is not able to detect the text in subway.jpg or burberry.jpg. I added edge detection, which seems to be working, but not solving issues.

--- detect.py
+++ detect.py
@@ -1,21 +1,27 @@
 import cv2
 import pytesseract
-# C:\Program Files\Tesseract-OCR\tesseract.exe
+import numpy as np
 
 # Mention the installed location of Tesseract-OCR in your system
 pytesseract.pytesseract.tesseract_cmd = 'System_path_to_tesseract.exe'
 
 # Read image from which text needs to be extracted
 img = cv2.imread("sample.jpg")
+# img = cv2.imread("burberry.jpg")
+# img = cv2.imread("subway.jpg")
 
   
 # Preprocessing the image starts
   
 # Convert the image to gray scale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  
+# cv2.imshow('gray',gray)
+# cv2.waitKey()
+
 # Performing OTSU threshold
 ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+# cv2.imshow('thresh1',thresh1)
+# cv2.waitKey()
   
 # Specify structure shape and kernel size. 
 # Kernel size increases or decreases the area 
@@ -26,6 +32,42 @@ rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
   
 # Appplying dilation on the threshold image
 dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+# cv2.imshow('dilation',dilation)
+# cv2.waitKey()
+
+
+############# Edge-Detection for Contours #############
+# # converting BGR to HSV
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+# # define range of red color in HSV
+# lower_red = np.array([30,150,50])
+# upper_red = np.array([255,255,180])
+    
+# # create a red HSV colour boundary and 
+# # threshold HSV image
+# mask = cv2.inRange(hsv, lower_red, upper_red)
+
+# # Bitwise-AND mask and original image
+# res = cv2.bitwise_and(img,img, mask= mask)
+
+# # Display an original image
+# cv2.imshow('Original',img)
+
+# # finds edges in the input image image and
+# # marks them in the output map edges
+# edges = cv2.Canny(img,200,300)
+
+# # Display edges in a frame
+# cv2.imshow('Edges',edges)
+
+# # Wait for Esc key to stop
+# cv2.waitKey()
+# contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, 
+#                                                  cv2.CHAIN_APPROX_NONE)
+# contours = sorted(contours, key=cv2.contourArea, reverse=True)
+#####################################################
+
   
 # Finding contours
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
@@ -51,6 +93,8 @@ for cnt in contours:
       
     # Cropping the text block for giving input to OCR
     cropped = im2[y:y + h, x:x + w]
+    # cv2.imshow('cropped',cropped)
+    # cv2.waitKey()
       
     # Open the file in append mode
     file = open("recognized.txt", "a")
