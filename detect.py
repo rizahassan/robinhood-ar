8dca675f2bcb1354aca3813c5c0c019b4317521b
roshanpoduval <roshan.poduval@gmail.com>
2021-04-09 17:11:39 -0500

    Edge fill
    Added some edge filling code we could try working with

--- detect.py
+++ detect.py
@@ -68,10 +68,53 @@ dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
 # contours = sorted(contours, key=cv2.contourArea, reverse=True)
 #####################################################
 
+
   
-# Finding contours
+# Finding contours (comment out if above edge detection is used)
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
                                                  cv2.CHAIN_APPROX_NONE)
+
+#####################FILL EDGES#############################
+# # load image
+# fg_img = edges
+
+# # threshold
+# fg_thresh = cv2.threshold(fg_img, 128, 255, cv2.THRESH_BINARY)[1]
+
+# # apply close morphology
+# #fg_kernel = np.ones((5,5), np.uint8)
+# fg_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+# fg_thresh = cv2.morphologyEx(fg_thresh, cv2.MORPH_CLOSE, fg_kernel)
+
+# # get bounding box coordinates from the one filled external contour
+# fg_filled = np.zeros_like(fg_thresh)
+# fg_contours = cv2.findContours(fg_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# fg_contours = fg_contours[0] if len(fg_contours) == 2 else fg_contours[1]
+# fg_the_contour = fg_contours[0]
+# fg_x,fg_y,fg_w,fg_h = cv2.boundingRect(fg_the_contour)
+# cv2.drawContours(fg_filled, [fg_the_contour], 0, 255, -1)
+
+# # crop filled contour image
+# fg_result = fg_filled.copy()
+# fg_result = fg_result[fg_y:fg_y+fg_h, fg_x:fg_x+fg_w]
+
+
+# # display results
+# cv2.imshow("THRESH", fg_thresh)
+# cv2.imshow("FILLED", fg_filled)
+# cv2.imshow("CROPPED", fg_result)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+########################################################################
+
+# # Try filling the contours made from edges
+# my_img = img
+# my_img = np.zeros_like(my_img)
+# cv2.fillPoly(my_img, pts = contours, color=(255,255,255))
+# cv2.imshow("my_img", my_img)
+# cv2.waitKey()
+# exit(0)
+
   
 # Creating a copy of image
 im2 = img.copy()
