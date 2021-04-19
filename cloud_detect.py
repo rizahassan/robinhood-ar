64965499fefe5e1f6f55037b6f6199d60371f7a6
roshanpoduval <roshan.poduval@gmail.com>
2021-04-19 14:04:41 -0500

    Integrate with rh ticker api
    This now searches for the ticker associated with the text in the image. If it finds a match, then it returns the text from the image that had a ticker match.

--- cloud_detect.py
+++ cloud_detect.py
@@ -11,10 +11,30 @@ def detect_text(path):
  
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
+
+    # print('Texts len=' + str(texts.__len__()) + ' :')
+    # max_len = -1
+    # for ele in texts:
+    #     if len(str(ele.description)) > max_len:
+    #         max_len = len(str(ele.description))
+    #         res = ele.description
+    # second_max_len = -1
+    # for ele in texts:
+    #     if len(str(ele.description)) > second_max_len and len(str(ele.description)) != max_len:
+    #         second_max_len = len(str(ele.description))
+    #         res2 = ele.description
+    # if max_len != -1: print(res)
+    # if second_max_len != -1: print(str(res2).lower())
+    # if second_max_len != -1: return str(res2).lower()
 
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        if len(str(text.description)) <= 1: continue
+        # print("Searching for: " + str(text.description))
+        if try_rh_search(str(text.description)):
+            # print(str(text.description).lower().strip())
+            return str(text.description).lower().strip() # will check the robinhood ticker search api and if it finds a match, then returns
+        else:
+            continue
 
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in text.bounding_poly.vertices])
@@ -28,10 +48,34 @@ def detect_text(path):
                 response.error.message))
 
 
+def try_rh_search(text):
+    import robin_stocks.robinhood as rh
+    # import robin_stocks.gemini as gem
+    # import robin_stocks.tda as tda
+    # import pprint
+    login = rh.login("roshan.poduval@gmail.com", "testpassword579")
+    stock = None
+    stock = rh.stocks.find_instrument_data(text)
+    if stock[0] is None:
+        return False
+    else:
+        # stock = stock[0]['symbol']
+        # stockPrice = ' '.join(rh.stocks.get_latest_price(stock))
+        # print("Current price of " + stock + ": $" + stockPrice)
+        # print("The general information about the " + stock + " stock:")
+        # # pprint.pprint(rh.stocks.get_fundamentals(stock))
+        return True
+
+
 def main():
     # detect_text("sample.jpg")
-    detect_text("burberry.jpg")
-    # detect_text("subway.jpg")
+    # assert detect_text("burberry.jpg") == 'burberry' # seems like skewed text also has a problem ## output = burberr
+    
+    assert detect_text("subway.jpg") == None # since subway is not a publically traded company
+    assert detect_text("mcdonalds.jpg") == "mcdonald's"
+    # assert detect_text("starbucks.png") == "starbucks" # seems like curved text causes problems ## output = coffee
+    assert detect_text("amazon.jpg") == "amazon"
+    assert detect_text("microsoft.jpg") == "microsoft"
 
 
 if __name__ == "__main__":
