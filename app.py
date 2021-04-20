5148b3c122a32f3a83a5bb93aa28eebb4f2fd18e
Riza Hassan <55040840+rizahassan@users.noreply.github.com>
2021-04-20 17:32:55 -0500

    Add routes to save snapshot

--- app.py
+++ app.py
@@ -5,20 +5,48 @@ import robin_stocks.gemini as gem
 import robin_stocks.tda as tda
 import pprint
 import sys
+from PIL import Image
+import PIL
+import io
+import base64
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
-
+# Host URL
 url = "http://localhost:5000"
 
-
+# Route to the landing page
 @app.route('/')
 def login_page():
     session['auth'] = True
     return render_template('login.html')
 
+# GET HTTP REQUEST
+# URL : http://localhost:5000/image_info
+# Save the snapshot from the webcam to the camera-image directory
+@app.route("/image_info",methods= ['GET'])
+def image_info():
+    # Get the image in URI Base 64 format
+    myfile= request.args.get('myimage').split(',')
+    imgdata = base64.b64decode(myfile[1])
+    im = Image.open(io.BytesIO(imgdata))
+
+    # Save the latest snapshot to the camera-image directory in a .png format
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    fileName="image.png"
+    imagePath =os.path.join(__location__, 'camera-image/image.png')
+    im.save(imagePath,'png')
+
+    width, height = im.size
+    imgformat=im.format
+    return jsonify(width=width,height=height,imgformat=imgformat)
+
+
+# HTTP REQUEST
+# URL : http://localhost:5000/authenticate
 # Authenticate the Robinhood username and password
-@app.route('/authenticate',methods=['POST','GET'])
+@app.route('/authenticate',methods=['POST'])
 def authenticate():
     # Login POST Request
     if request.method == 'POST':
@@ -42,15 +70,16 @@ def authenticate():
                 return redirect('/login-error')
         else:
             error = {'error':'Missing username or password'}
-            session['auth'] = False
+            session['auth'] = Falsegit 
             return redirect('/login-error')
 
+# Route to login error page
 @app.route('/login-error')
 def loginerror_page():
     session['auth'] = False
     return render_template('loginerror.html')
 
-
+# Route to access the Augmented Reality
 @app.route('/view')
 def viewing_page():
     auth = session.get('auth',False)
