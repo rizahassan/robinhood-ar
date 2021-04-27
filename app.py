7d2823a26124334e887573faf14e735aa3a0e765
dinethG2 <pgunawardena@wisc.edu>
2021-04-26 19:33:31 -0500

    Updated app.py to use get request for stock information

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, current_app,jsonify,request,redirect,render_template,session
+from flask import Flask, current_app, jsonify, request, redirect, render_template, session
 import robin_stocks.robinhood as rh
 import robin_stocks.gemini as gem
 import robin_stocks.tda as tda
@@ -11,7 +11,7 @@ import io
 import base64
 import logging
 import pyotp
-
+import requests
 
 # Disable log in Flask server
 log = logging.getLogger('werkzeug')
@@ -25,55 +25,89 @@ app.config.from_pyfile('config.py')
 url = "http://localhost:5000"
 
 # Route to the landing page
+
+
 @app.route('/')
 def login_page():
     session['auth'] = True
     return render_template('login.html')
 
 # Route to login error page
+
+
 @app.route('/login-error')
 def loginerror_page():
     session['auth'] = False
     return render_template('loginerror.html')
 
 # Route to access the Augmented Reality
+
+
 @app.route('/view')
 def viewing_page():
-    auth = session.get('auth',False)
-    if auth is True: 
+    auth = session.get('auth', False)
+    if auth is True:
         return render_template('index.html')
     else:
         return redirect('/')
 
+
+# GET HTTP REQUEST
+# URL : http://localhost:5000/stock_info
+stocks = []
+text_result = ""
+
+
+@app.route('/stock_info', methods=['GET'])
+def returnStock():
+
+   # for users first time logging in
+   # login = rh.login("roshan.poduval@gmail.com", "testpassword579")
+
+    stock = rh.stocks.find_instrument_data(text_result)
+
+    # Add stock price to dictionary
+    if stock[0] is not None:
+        stockPrice = (rh.stocks.get_latest_price(stock[0]["symbol"]))
+        stock[0]['stockPrice'] = " ".join(stockPrice)
+
+        # delete any stock information if needed
+        # example: del stock[0]['url']
+
+    return jsonify({'stocks': stock[0]})
+
+
 # GET HTTP REQUEST
 # URL : http://localhost:5000/image_info
 # Save the snapshot from the webcam to the camera-image directory
-@app.route("/image_info",methods= ['GET'])
+
+
+@app.route("/image_info", methods=['GET'])
 def image_info():
     # Get the image in URI Base 64 format
-    myfile= request.args.get('myimage').split(',')
+    myfile = request.args.get('myimage').split(',')
     imgdata = base64.b64decode(myfile[1])
     im = Image.open(io.BytesIO(imgdata))
 
+    # Store and display text_detect output for get request
     from cloud_detect import text_detect
-    print(text_detect(imgdata))
+    text_detect_output = text_detect(imgdata)
+    print(text_detect_output)
 
-    # Save the latest snapshot to the camera-image directory in a .png format
-    __location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    fileName="image.png"
-    imagePath =os.path.join(__location__, 'camera-image/image.png')
-    # im.save(imagePath,'png')
-
-    width, height = im.size
-    imgformat=im.format
-    return jsonify(width=width,height=height,imgformat=imgformat)
+    # Retrieve text_detect output and send get request for stock information
+    if(text_detect_output != None):
+        global text_result
+        text_result = text_detect_output
+        stock_info = requests.get(url + '/stock_info')
+        return text_detect_output
+    else:
+        return jsonify(error="No text detected")
 
 
 # HTTP REQUEST
 # URL : http://localhost:5000/authenticate
 # Authenticate the Robinhood username and password
-@app.route('/authenticate',methods=['POST'])
+@app.route('/authenticate', methods=['POST'])
 def authenticate():
     # Login POST Request
     if request.method == 'POST':
@@ -84,15 +118,15 @@ def authenticate():
 
         # If username/password/mfaCode is not empty, then complete the authentication
         if (username or password or mfaCode) != '':
-            message = {'message':'User authenticated'}
-            mfaCode= mfaCode.encode("UTF-8")
-            mfaCode= base64.b32encode(mfaCode)
-            totp  = pyotp.TOTP(mfaCode).now()
-            
-            login = rh.login(username,password,expireMin,mfa_code=totp)
+            message = {'message': 'User authenticated'}
+            mfaCode = mfaCode.encode("UTF-8")
+            mfaCode = base64.b32encode(mfaCode)
+            totp = pyotp.TOTP(mfaCode).now()
+
+            # login = rh.login(username, password, expireMin, mfa_code=totp)
 
             # Get the access token from the robinhood login
-            access = login.get('access_token',None)
+            access = login.get('access_token', None)
 
             if(access is not None):
                 session['auth'] = True
@@ -101,22 +135,22 @@ def authenticate():
                 session['auth'] = False
                 return redirect('/login-error')
         else:
-            error = {'error':'Missing username or password'}
+            error = {'error': 'Missing username or password'}
             session['auth'] = False
             return redirect('/login-error')
 
 # POST HTTP REQUEST
 # URL : http://localhost:5000/login
 # Log out user from the Robinhood account and exit the AR view
-@app.route('/logout',methods=['POST'])
+
+
+@app.route('/logout', methods=['POST'])
 def logout():
     # Logout POST Request
     if request.method == 'POST':
         rh.logout()
         session['auth'] = False
         return redirect('/')
-
-
 
 
 if __name__ == '__main__':
