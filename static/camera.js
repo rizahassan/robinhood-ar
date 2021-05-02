e0337cddcab20db3d7561e77a637e64214b80b55
roshanpoduval <roshan.poduval@gmail.com>
2021-05-02 13:59:22 -0500

    Changed back to GET

--- static/camera.js
+++ static/camera.js
@@ -15,7 +15,7 @@ take_snapshot()
 function take_snapshot() {
 Webcam.snap(function (data_uri) {
 $.ajax({
-type: "POST",
+type: "GET",
 data: "myimage=" + encodeURIComponent(data_uri),
 url: "/image_info" ,
 contentType: false,
